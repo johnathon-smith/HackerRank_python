30759f08ea2010313043ef1218a7ae697432848e
Johnathon Smith <smith.johnathon01@gmail.com>
2021-10-02 13:43:44 -0500

    Added HackerRank python challenge 5

---
+++ exercise_5.py
@@ -0,0 +1,13 @@
+"""
+Task:
+The provided code stub reads an integer, n, from STDIN. For all non-negative integers i < n, print i^2.
+
+Input Format:
+The first and only line contains the integer, n.
+"""
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    
+for n in range(0, n):
+    print(n ** 2)
